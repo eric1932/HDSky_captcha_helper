19045f012a6ec62db8c6302410660ef101db6e42
Eric Liu <eric1932@users.noreply.github.com>
2022-01-30 15:40:28 -0600

    Remove unused routes

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
-from typing import Optional
+import re
 
 from fastapi import FastAPI
-import re
+
 import hdsky
 
 app = FastAPI()
@@ -12,14 +12,15 @@ def read_root():
     return {"Hello": "World"}
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Optional[str] = None):
-    return {"item_id": item_id, "q": q}
+# @app.get("/items/{item_id}")
+# def read_item(item_id: int, q: Optional[str] = None):
+#     return {"item_id": item_id, "q": q}
 
 
 @app.get("/hdsky/captcha/{image_hash}")
-def hdsky_captcha(image_hash: str, q: Optional[str] = None):
+def hdsky_captcha(image_hash: str):
     # check hash (eg. 2f92cc2f6184b147fed10433d6b64e57)
+    # 91b2162cc946786a19a1c4659706159f = 6BB372
     if not re.match(r"[a-z0-9]{32}", image_hash):
         return {"status": 1, "message": "invalid hash"}
     full_url = f"https://hdsky.me/image.php?action=regimage&imagehash={image_hash}"
